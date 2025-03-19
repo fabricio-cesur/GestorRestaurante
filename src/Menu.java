42f32cac0bb0b7ad818573cf3ff826b3e4839ab3
Fabricio Oliva <fabricio.s399523@cesurformacion.com>
2025-03-19 09:48:16 +0100

    refactor: Menu de cada clase
    Se hizo un método para cada clase y se cambió el Restaurante y Scanner a
    tipo public static para poder usarlo en las demás funciones del archivo
    .java
    Nota: Se podría refactorizar más en un sólo método con el atributo
    siendo la clase de la que será el menú.

--- src/Menu.java
+++ src/Menu.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Menu {
     public static boolean vaciar = true;
+    public static Scanner sc = new Scanner(System.in);
+    public static Restaurante res = new Restaurante();
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         
-        Restaurante res = new Restaurante();
 
-        Scanner sc = new Scanner(System.in);
         String opcion;
         vaciar_consola();
         System.out.println("Bienvenido al Gestor del Restaurante");
@@ -27,37 +27,13 @@ public class Menu {
             
             switch (opcion) {
                 case "1", "mesas", "MESAS" -> {
-                    vaciar_consola();
-                    System.out.println("¿Qué quiere hacer con las mesas?");
-                    System.out.println("1. Registrar Mesa");
-                    System.out.println("2. Modificar Mesa");
-                    System.out.println("3. Eliminar Mesa");
-                    System.out.println("0. Salir");
-                    System.out.print(">>> ");
-                    opcion = sc.next();
-                    vaciar_consola();
+                    menu_mesas();
                 }
                 case "2", "platos", "PLATOS" -> {
-                    vaciar_consola();
-                    System.out.println("¿Qué quiere hacer con los platos?");
-                    System.out.println("1. Registrar Plato");
-                    System.out.println("2. Modificar Plato");
-                    System.out.println("3. Eliminar Plato");
-                    System.out.println("0. Salir");
-                    System.out.print(">>> ");
-                    opcion = sc.next();
-                    vaciar_consola();
+                    menu_platos();
                 }
                 case "3", "pedidos", "PEDIDOS" -> {
-                    vaciar_consola();
-                    System.out.println("¿Qué quiere hacer con las pedidos?");
-                    System.out.println("1. Registrar Pedido");
-                    System.out.println("2. Modificar Pedido");
-                    System.out.println("3. Eliminar Pedido");
-                    System.out.println("0. Salir");
-                    System.out.print(">>> ");
-                    opcion = sc.next();
-                    vaciar_consola();
+                    menu_pedidos();
                 }
                 case "4" -> {
                     //TODO: Todo el funcionamiento de descuentos
@@ -83,6 +59,103 @@ public class Menu {
         } while (!opcion.equals("0"));
         sc.close();
     }
+
+    public static void menu_mesas() {
+        String opcion;
+        do {
+            vaciar_consola();
+            System.out.println("¿Qué quiere hacer con las mesas?");
+            System.out.println("1. Registrar Mesa");
+            System.out.println("2. Modificar Mesa");
+            System.out.println("3. Eliminar Mesa");
+            System.out.println("0. Salir");
+            System.out.print(">>> ");
+            opcion = sc.next();
+            switch (opcion) {
+                case "1", "registrar", "REGISTRAR" -> {
+                    res.registrarMesa();
+                }
+                case "2", "modificar", "MODIFICAR" -> {
+                    //res.modificarMesa();
+                }
+                case "3", "eliminar", "ELIMINAR" -> {
+                    //res.eliminarMesa();
+                }
+                case "0" -> {
+                    //salir
+                }
+                default -> {
+                    System.out.println("ERR0R: Comando no reconocido");
+                }
+            }
+        } while (!opcion.equals("0"));
+        vaciar_consola();
+    }
+
+    public static void menu_platos() {
+        String opcion;
+        do {
+            vaciar_consola();
+            System.out.println("¿Qué quiere hacer con las platos?");
+            System.out.println("1. Registrar Plato");
+            System.out.println("2. Modificar Plato");
+            System.out.println("3. Eliminar Plato");
+            System.out.println("0. Salir");
+            System.out.print(">>> ");
+            opcion = sc.next();
+            switch (opcion) {
+                case "1", "registrar", "REGISTRAR" -> {
+                    res.registrarPlato();
+                }
+                case "2", "modificar", "MODIFICAR" -> {
+                    //res.modificarPlato();
+                }
+                case "3", "eliminar", "ELIMINAR" -> {
+                    //res.eliminarPlato();
+                }
+                case "0" -> {
+                    //salir
+                }
+                default -> {
+                    System.out.println("ERR0R: Comando no reconocido");
+                }
+            }
+        } while (!opcion.equals("0"));
+        vaciar_consola();
+    }
+
+    public static void menu_pedidos() {
+        String opcion;
+        do {
+            vaciar_consola();
+            System.out.println("¿Qué quiere hacer con los pedidos?");
+            System.out.println("1. Registrar Pedidos");
+            System.out.println("2. Modificar Pedidos");
+            System.out.println("3. Eliminar Pedidos");
+            System.out.println("0. Salir");
+            System.out.print(">>> ");
+            opcion = sc.next();
+            switch (opcion) {
+                case "1", "registrar", "REGISTRAR" -> {
+                    res.registrarPedido();
+                }
+                case "2", "modificar", "MODIFICAR" -> {
+                    //res.modificarPedido();
+                }
+                case "3", "eliminar", "ELIMINAR" -> {
+                    //res.eliminarPedido();
+                }
+                case "0" -> {
+                    //salir
+                }
+                default -> {
+                    System.out.println("ERR0R: Comando no reconocido");
+                }
+            }
+        } while (!opcion.equals("0"));
+        vaciar_consola();
+    }
+
 
     public static void vaciar_consola() {
         String os = System.getProperty("os.name");
